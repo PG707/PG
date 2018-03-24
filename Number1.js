47e078ff1e98fb7d47141f1855bb12ed7395e5bf
PG707 <37436709+PG707@users.noreply.github.com>
2018-03-24 17:48:53 +0700

    Update Number1.js

--- Number1.js
+++ Number1.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/p/paket1.html"}
 if(x=="2"){xxx="/p/paket2.html"}
 if(x=="3"){xxx="/p/paket3.html"}
 if(x=="4"){xxx="/p/paket4.html"}
-if(x=="5"){xxx="/p/paket5.html"}a_to_ve[i].href="https://andakepoin.blogspot.com"+xxx+"?lnk="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/p/paket5.html"}a_to_ve[i].href="https://andakepoin.blogspot.com"+xxx+"?xyz="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
